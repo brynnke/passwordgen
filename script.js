8b73f809b1b91c690ad48e3721a7e37e63090c24
Second <second@Brynns-MacBook-Air.local>
2022-03-22 21:08:18 -0500

    new files

---
+++ script.js
@@ -0,0 +1,29 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
